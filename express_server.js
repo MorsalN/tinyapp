3c1752440a8502bc83577dfff98d249d13d124f3
Morsal Niyaz <morsalniyaz@gmail.com>
2021-11-08 20:45:27 -0800

    Added short URL function

--- express_server.js
+++ express_server.js
@@ -1,7 +1,9 @@
+const bodyParser = require("body-parser");
 const { response, request } = require('express');
 const express = require('express');
 const app = express();
 const PORT = 8080;
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.set('view engine', 'ejs');
 
@@ -9,6 +11,17 @@ const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+
+function generateRandomString(n) {
+  let result = '';
+  let characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  for ( let i = 0; i < n; i++ ) {
+    result += characters.charAt(Math.floor(Math.random()*characters.length));
+ }
+ console.log(result);
+ return result;
+}
+generateRandomString(6) 
 
 
 app.get('/', (request, response) => {
@@ -20,9 +33,18 @@ app.get('/urls', (request, response) => {
   response.render('urls_index', templateVars);
 });
 
+app.post("/urls", (request, response) => {
+  console.log(request.body);  // Log the POST request body to the console
+  response.send("Ok");         // Respond with 'Ok' (we will replace this)
+});
+
 app.get('/hello', (request, response) => {
   const templateVars = { greeting: 'Hello World!' };
   response.render('hello_world', templateVars);
+});
+
+app.get('/urls/new', (request, response) => {
+  response.render('urls_new');
 });
 
 app.get("/urls/:shortURL", (request, response) => {
