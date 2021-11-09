bf73734f79952d5a5f1b1d97c06e6df7d35108c8
Morsal Niyaz <morsalniyaz@gmail.com>
2021-11-09 14:59:22 -0800

    Added comments

--- express_server.js
+++ express_server.js
@@ -1,12 +1,14 @@
 const bodyParser = require("body-parser");
-const { response, request } = require('express');
+// const { response, request } = require('express');
 const express = require('express');
 const app = express();
 const PORT = 8080;
 app.use(bodyParser.urlencoded({extended: true}));
 
+//this is saying what engine to use (view) and to look at the ejs file in views
 app.set('view engine', 'ejs');
 
+//starter data
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
@@ -22,21 +24,30 @@ function generateRandomString() {
  return result;
 }
 
+//example of an endpoint
 app.get('/', (request, response) => {
   response.send('Hello!');
 });
 
+//GET 
+//urls is a key for the obj template vars 
 app.get('/urls', (request, response) => {
-  const templateVars = { urls : urlDatabase }; //where is urls coming from?
+  const templateVars = { urls : urlDatabase}; 
   response.render('urls_index', templateVars);
 });
 
+//POST the new shortURL
 app.post("/urls", (request, response) => {
-  const shortStr = generateRandomString();
-  urlDatabase[shortStr] = request.body['longURL']; //urlDatabase[shortStr]? 
-  console.log(urlDatabase[shortStr]);
+  const shortURL = generateRandomString();
+  //request.body is an obj so we want to get the longURL key
+  const longURL = request.body.longURL;
+  console.log('before: ', urlDatabase);
+  //adding a new key called shortURL and making it equal to the value longURL
+  urlDatabase[shortURL] = longURL;
+  console.log('after: ', urlDatabase);
+  // console.log(urlDatabase[shortStr]);
   // console.log(request.body);  // Log the POST request body to the console
-  response.redirect(`/urls/${shortStr}`); //goes to the url with the new shortURL
+  response.redirect(`/urls/${shortURL}`); //sending another get request 
 });
 
 app.get('/urls/new', (request, response) => {
@@ -45,28 +56,26 @@ app.get('/urls/new', (request, response) => {
 });
 
 //this comes after urls new or else it will go to this page first
+//the : means getting the value of shortURL
 app.get("/urls/:shortURL", (request, response) => {
+  // console.log('request.params: ', request.params);
   const shortURL = request.params.shortURL;
+  //the key longURL will get the value of longURL by urlDatabase[shortURL]
   const templateVars = { shortURL: shortURL, longURL: `${urlDatabase[shortURL]}`};
   response.render("urls_show", templateVars);
 });
 
-app.get('/hello', (request, response) => {
-  const templateVars = { greeting: 'Hello World!' };
-  response.render('hello_world', templateVars);
-});
-
-
+//this endpoint will get you straight to the website
 app.get("/u/:shortURL", (request, response) => {
   const longURL = urlDatabase[request.params.shortURL]; //request.params
   if (longURL) {
-    response.status(307).redirect('http://' + longURL);
+    response.status(307).redirect(longURL);
   } else {
     response.status(404).send("404 page not found");
   }
 });
 
-
+//this shows what is being added to the urlDatabase as an object
 app.get('/urls.json', (request, response) => {
   response.json(urlDatabase)
 });
@@ -88,5 +97,11 @@ app.listen(PORT, () => {
 
 // app.get('/fetch', (request, response) => {
 //   response.send(`a = ${a}`);
+// });
+
+
+// app.get('/hello', (request, response) => {
+//   const templateVars = { greeting: 'Hello World!' };
+//   response.render('hello_world', templateVars);
 // });
 
