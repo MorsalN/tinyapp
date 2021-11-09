51c9359c4a5396660bd338508b8679e6169eca84
Morsal Niyaz <morsalniyaz@gmail.com>
2021-11-08 16:42:46 -0800

    Set ejs as the view engine

--- express_server.js
+++ express_server.js
@@ -3,10 +3,13 @@ const express = require('express');
 const app = express();
 const PORT = 8080;
 
+app.set('view engine', 'ejs');
+
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+
 
 app.get('/', (request, response) => {
   response.send('Hello!');
